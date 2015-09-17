de0c2517559e07985eb11e71254f3d2c1f2e2e03
nicothebrush <nicola.riolini@gmail.com>
2015-09-17 08:52:50 +0200

    End report with conformed

--- quality_statistic/report/status_parser.py
+++ quality_statistic/report/status_parser.py
@@ -63,7 +63,7 @@ class Parser(report_sxw.rml_parse):
     # --------
     # Utility:
     # --------
-    def _get_domain(self, data=None, description=False):
+    def _get_domain(self, data=None, description=False, field='date'):
         ''' Get domain from data passed with wizard
             create domain for search filter depend on data
             if description is True, return description of filter instead 
@@ -81,10 +81,10 @@ class Parser(report_sxw.rml_parse):
             res = []
             if data.get('from_date', False):
                 res.append(
-                    ('date', '>=', '%s 00:00:00' % data['from_date']))
+                    (field, '>=', '%s 00:00:00' % data['from_date']))
             if data.get('to_date', False):
                 res.append(
-                    ('date', '<=', '%s 23:59:59' % data['to_date']))
+                    (field, '<=', '%s 23:59:59' % data['to_date']))
         return res
         
     # ------------------
@@ -136,7 +136,8 @@ class Parser(report_sxw.rml_parse):
             context = {}
             context['lang'] = 'it_IT'
             
-            for item in claim_pool.browse(self.cr, self.uid, claim_ids, context):
+            for item in claim_pool.browse(self.cr, self.uid, claim_ids, 
+                    context):
                 self.totals['claims'] += 1
                 
                 # --------------
@@ -172,40 +173,41 @@ class Parser(report_sxw.rml_parse):
         
         if block_type == 'conformed':
             # Reset totals:
-            self.totals['not_conformed'] = 0
+            self.totals['conformed'] = 0
 
-            domain = self._get_domain(data)
+            domain = self._get_domain(data, field='insert_date')
             domain.append(('state', 'not in', ('draft', 'cancel')))
-            claim_pool = self.pool.get('quality.conformed')
-            claim_ids = claim_pool.search(self.cr, self.uid, domain)
+            conformed_pool = self.pool.get('quality.conformed')
+            conformed_ids = conformed_pool.search(self.cr, self.uid, domain)
 
             res = {
                 'Origine': {},
-                'Cause': {},
                 "Gravita'": {},
                 }
+            dictionary = {  # Italy:
+                False: 'Nessuna',
+                'claim': 'Reclamo',
+                'acceptation': 'Accettazione',
+                'sampling': 'Campionamento',
+                'packaging': 'Confezionamento',
+                'other': 'Altro',
+                }    
 
             # Language:
             context = {}
             context['lang'] = 'it_IT'
             
-            for item in claim_pool.browse(self.cr, self.uid, claim_ids, context):
-                self.totals['claims'] += 1
+            for item in conformed_pool.browse(self.cr, self.uid, conformed_ids, 
+                    context):
+                self.totals['conformed'] += 1
                 
                 # --------------
                 # Caracteristic:
                 # --------------
                 # Origin:
                 block = res['Origine'] # for fast replace
-                name = _(item.origin_id.name) if item.origin_id else 'Nessuna'
+                name = dictionary[item.origin]# if item.origin else 'Nessuna'
                 
-                if name not in block: # Create totalizer
-                    block[name] = 0                
-                block[name] += 1
-                
-                #  Cause
-                block = res['Cause'] # for fast replace
-                name = _(item.cause_id.name) if item.cause_id else 'Nessuna'
                 if name not in block: # Create totalizer
                     block[name] = 0                
                 block[name] += 1
@@ -216,14 +218,6 @@ class Parser(report_sxw.rml_parse):
                 if name not in block: # Create totalizer
                     block[name] = 0                
                 block[name] += 1
-                
-                # -------
-                # Totals:
-                # -------
-                if item.conformed_id:
-                    self.totals['not_conformed'] += 1
-        
-                
         return res
     
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
